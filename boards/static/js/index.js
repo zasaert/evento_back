16ca8dc34d699f59a28eb5ee27c3a7cd5dc79064
zasaert <155296671+zasaert@users.noreply.github.com>
2024-04-01 17:28:00 +0300

    deleted updateStatus

--- boards/static/js/index.js
+++ boards/static/js/index.js
@@ -19,7 +19,6 @@ for (list of lists) {
 			}
 			let name = e.target.textContent
 			let status = 0
-			updateStatus(name,status)
 			selected = null
 
 		})
@@ -34,7 +33,6 @@ for (list of lists) {
 			}
 			let status = 2
 			let name = selected.textContent
-			updateStatus(name,status)
 			selected = null
 			
 		})
@@ -48,48 +46,12 @@ for (list of lists) {
 			}
 			let status = 1
 			let name = selected.textContent
-			updateStatus(name,status)
+			
 			selected = null
 			
 		})
 	})
 }
-function updateStatus(name, newStatus) { 
-	// Подключение к базе данных
-	const sqlite3 = require('sqlite3').verbose();
-	let db = new sqlite3.Database('../db.sqlite3', (err) => {
-	  if (err) {
-		console.error('Ошибка подключения к базе данных:', err.message);
-		return;
-	  }
-	  console.log('Подключено к базе данных.');
-	  // Подготовка запроса
-	  const stmt = db.prepare("UPDATE boards_card SET status = ? WHERE name = ?");
-  
-	  // Выполнение запроса
-	  stmt.run(newStatus, name, (err) => {
-		if (err) {
-		  console.error('Ошибка обновления статуса:', err.message);
-		} else {
-		  console.log(`Статус строки с name ${id} обновлен на ${newStatus}`);
-		}
-  
-		// Закрытие подготовленного запроса
-		stmt.finalize((err) => {
-		  if (err) {
-			console.error('Ошибка завершения запроса:', err.message);
-		  }
-		});
-	  });
-	});
-  
-	 db.close((err) => {
-	   if (err) {
-	     console.error('Ошибка закрытия соединения с базой данных:', err.message);
-	   } else {
-	    console.log('Соединение с базой данных закрыто.');
-	 }
-	 });
 
 function openPopup() {
 	var popup = document.getElementById('popup')
@@ -105,4 +67,3 @@ function closePopup() {
 //для левого столбца
 let center_list = document.querySelectorAll('#center.list')
 console.log(center_list)
-}
